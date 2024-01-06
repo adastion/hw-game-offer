e3e9f9dd1b613de039e670711c82297948326e47
Andrey Sery <andrey.72006@gmail.com>
2024-01-07 01:13:59 +0300

    corrected the names in the data GAME_STATE

--- ui/winning/winning.component.js
+++ ui/winning/winning.component.js
@@ -1,6 +1,6 @@
 export function WinGame() {
   const containerElement = document.createElement("div");
-  containerElement.textContent = "You Winning!!!";
+  containerElement.textContent = "You Win!!!";
 
   return containerElement;
 }
