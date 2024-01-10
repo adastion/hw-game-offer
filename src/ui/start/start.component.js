a2fc81667805806af6c6371fd40e1c334e149b0d
Andrey Sery <andrey.72006@gmail.com>
2024-01-11 00:40:20 +0300

    button styles

--- src/ui/start/start.component.js
+++ src/ui/start/start.component.js
@@ -2,6 +2,7 @@ import { start } from "../../data/game.data.js";
 
 export function StartGeme() {
   const buttonStart = document.createElement("button");
+  buttonStart.classList.add("btn", "btn--start");
   buttonStart.textContent = "Start game";
   buttonStart.addEventListener("click", start);
 
