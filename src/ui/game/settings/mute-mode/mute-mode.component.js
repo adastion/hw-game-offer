c9235b673d602d8acde75e3b3fb869508ad59230
Andrey Sery <andrey.72006@gmail.com>
2024-01-21 00:52:11 +0300

    styling mute mode

--- src/ui/game/settings/mute-mode/mute-mode.component.js
+++ src/ui/game/settings/mute-mode/mute-mode.component.js
@@ -1,12 +1,24 @@
 import { setStatusSounds } from "../../../../data/game.data.js";
 
 export function MuteMode() {
+  const settingsItem = document.createElement("div");
+  settingsItem.classList.add("select__title")
+  settingsItem.textContent = "Mute mode";
+
+
+  const labelElement = document.createElement("label");
+  labelElement.classList.add("select__title--mute-mod")
+
   const muteModeElement = document.createElement("input");
   muteModeElement.type = "checkbox";
+  labelElement.append(muteModeElement)
 
   muteModeElement.addEventListener("change", (e) => {
     setStatusSounds(e.target.checked);
+    labelElement.classList.toggle("active")
   });
 
-  return muteModeElement;
+  settingsItem.append(labelElement)
+
+  return settingsItem;
 }
--- src/ui/start/start.component.js
+++ src/ui/start/start.component.js
@@ -2,7 +2,7 @@ import { start } from "../../data/game.data.js";
 
 export function StartGeme() {
   const buttonStart = document.createElement("button");
-  buttonStart.classList.add("btn", "btn--start");
+  buttonStart.classList.add("btn");
   buttonStart.textContent = "Start game";
   buttonStart.addEventListener("click", start);
 
