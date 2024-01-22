42b0140978e7cd005573dc2eb1f7a78626ef6192
Andrey Sery <andrey.72006@gmail.com>
2024-01-22 23:52:20 +0300

    fix

--- src/data/game.data.js
+++ src/data/game.data.js
@@ -27,7 +27,7 @@ const _data = {
     isMuted: SWITCHING_SOUNDS.off,
   },
   gemeTime: "",
-  gameStatus: GAME_STATE.game,
+  gameStatus: GAME_STATE.beginning,
   offerStatus: OFFER_STATUSES.default,
   coords: {
     current: {
